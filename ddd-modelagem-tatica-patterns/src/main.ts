d33c11e271708c0e364d49a2f8136f2214015ccd
Matheus G. Dias <matheusgiga123@gmail.com>
2022-04-28 13:46:31 -0300

    DDD modelagem: adicionando config para vscode rodar jest

--- ddd-modelagem-tatica-patterns/src/main.ts
+++ ddd-modelagem-tatica-patterns/src/main.ts
@@ -7,7 +7,7 @@ const customer = new Customer("123", "Matheus G.");
 customer.address = new Address("Lathi Heights", 1817, "MF", "Rijogo");
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 12.5);
-const item2 = new OrderItem("2", "Item 2", 50.7);
+const item1 = new OrderItem("1", "Item 1", 12.5, "p1", 1);
+const item2 = new OrderItem("2", "Item 2", 50.7, "p2", 2);
 
 const order = new Order("456", customer.id, [item1, item2]);
