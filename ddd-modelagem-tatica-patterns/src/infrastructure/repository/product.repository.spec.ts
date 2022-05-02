0d0531b7bcda4b6f56f0bc7c89e340ec55428157
Matheus G. Dias <matheusgiga123@gmail.com>
2022-05-02 13:33:08 -0300

    DDD modelagem: adicionando sequelize

---
+++ ddd-modelagem-tatica-patterns/src/infrastructure/repository/product.repository.spec.ts
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize-typescript";
+
+import { ProductModel } from "../db/sequelize/model/product.model";
+
+describe("Product repository test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logQueryParameters: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ProductModel]);
+
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should ", () => {});
+});
