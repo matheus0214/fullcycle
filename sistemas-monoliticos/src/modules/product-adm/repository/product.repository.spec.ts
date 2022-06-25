79001ce831e8ffc061cc400bb56a99695347a62a
Matheus G. Dias <matheusgiga123@gmail.com>
2022-06-25 15:35:31 -0300

    Sistemas monoliticos: implementando repositorio

--- sistemas-monoliticos/src/modules/product-adm/repository/product.repository.spec.ts
+++ sistemas-monoliticos/src/modules/product-adm/repository/product.repository.spec.ts
@@ -1,10 +1,14 @@
 import { Sequelize } from "sequelize-typescript";
+
+import { Id } from "../../@shared/domain/value-object/id.value-object";
+import { Product } from "../domain/product.entity";
+import { ProductRepository } from "./product.repository";
 import { ProductModel } from "./produt.model";
 
 describe("Product repository test", () => {
   let sequelize: Sequelize;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     sequelize = new Sequelize({
       dialect: "sqlite",
       storage: ":memory:",
@@ -13,9 +17,59 @@ describe("Product repository test", () => {
     });
 
     sequelize.addModels([ProductModel]);
+
+    await sequelize.sync();
   });
 
   afterEach(async () => {
     sequelize.close();
+  });
+
+  it("should create a product", async () => {
+    const productProps = {
+      id: new Id("1"),
+      name: "Product 1",
+      description: "Description",
+      purchasePrice: 120,
+      stock: 1,
+    };
+
+    const product = new Product(productProps);
+
+    const productRepository = new ProductRepository();
+
+    await productRepository.add(product);
+
+    const productDb = await ProductModel.findOne({
+      where: { id: productProps.id.id },
+    });
+
+    expect(productDb?.id).toEqual(productProps.id.id);
+    expect(productDb?.name).toEqual(productProps.name);
+    expect(productDb?.description).toEqual(productProps.description);
+    expect(productDb?.purchasePrice).toEqual(productProps.purchasePrice);
+    expect(productDb?.stock).toEqual(productProps.stock);
+  });
+
+  it("should find a product", async () => {
+    const productRepository = new ProductRepository();
+
+    await ProductModel.create({
+      id: "1",
+      name: "Product 1",
+      description: "Description",
+      purchasePrice: 120,
+      stock: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    const product = await productRepository.find("1");
+
+    expect(product?.id.id).toEqual("1");
+    expect(product?.name).toEqual("Product 1");
+    expect(product?.description).toEqual("Description");
+    expect(product?.purchasePrice).toEqual(120);
+    expect(product?.stock).toEqual(1);
   });
 });
---
+++ sistemas-monoliticos/src/modules/product-adm/repository/product.repository.ts
@@ -0,0 +1,45 @@
+import { ProductModel } from "./produt.model";
+
+import { Id } from "../../@shared/domain/value-object/id.value-object";
+import { Product } from "../domain/product.entity";
+import { IProductGateway } from "../gateway/product.gateway";
+
+export class ProductRepository implements IProductGateway {
+  async add(product: Product): Promise<void> {
+    try {
+      await ProductModel.create({
+        id: product.id.id,
+        name: product.name,
+        description: product.description,
+        purchasePrice: product.purchasePrice,
+        stock: product.stock,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async find(id: string): Promise<Product> {
+    const product = await ProductModel.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!product) {
+      throw new Error(`Product with id: ${id} not found`);
+    }
+
+    return new Product({
+      id: new Id(product.id),
+      name: product.name,
+      description: product.description,
+      purchasePrice: product.purchasePrice,
+      stock: product.stock,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
+}
